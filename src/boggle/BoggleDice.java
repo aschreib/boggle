5c31d8ed44fdd0d160abf5615118f62aed81676a
aschreib <aschreib@student.touro.edu>
2014-10-30 22:13:17 -0400

    first boggle push
    built outline of BoggleDice class, imported dictionary text file

---
+++ src/boggle/BoggleDice.java
@@ -0,0 +1,25 @@
+package boggle;
+
+public class BoggleDice {
+
+	/*
+	 * hard code the sixteen dice, each with the specific six letters provide a
+	 * random generator to choose one letter from each die
+	 * for a list of the 16 dice: www.everything2.com/title/Boggle
+	 */
+
+	private String[][] letterCubes;
+
+	public BoggleDice() {
+		// instantiate 2 dimensional array and hard-code letters in
+
+	}
+
+	public String[] getLetters() {
+		String[] letters = new String[16];
+		int counter = 0;
+		// uses random gen in a double for-loop to select a letter from each die
+		// and add it to the letters array
+		return letters;
+	}
+}
